a266ad9174120d6408a5b605a6120c215949f593
ducbet <ducbet1996@gmail.com>
2018-10-05 23:05:23 +0700

    login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
+    delete "/logout", to: "sessions#destroy"
     get  "/signup", to: "users#new"
+    get "/login", to: "sessions#new"
+    get "sessions/new"
+    post "/login", to: "sessions#create"
     post "/signup", to: "users#create"
     resources :games, only: [:index]
     resources :users, only: [:show]
