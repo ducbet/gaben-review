43df23e3462e8f4a31f69d381e8a10193aabfe2f
ducbet <ducbet1996@gmail.com>
2018-10-05 23:05:23 +0700

    home page feed data

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
+    @support = HomePageSupport.new Game.all.sample(6),
+                                    Game.all.sample(8),
+                                    Game.all.sample(9),
+                                    Game.all.sample(10),
+                                    Game.all.sample(12)
   end
 end
--- app/models/screenshot.rb
+++ app/models/screenshot.rb
@@ -1,3 +1,4 @@
 class Screenshot < ApplicationRecord
   belongs_to :game
+  mount_uploader :picture, PictureUploader
 end
