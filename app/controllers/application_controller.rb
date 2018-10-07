d8b32689c835b2bb41b5e03d3db853d0311967ef
phamgiacuong <phamgiacuong266@gmail.com>
2018-10-07 11:31:31 +0700

    user_profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,11 @@ class ApplicationController < ActionController::Base
     Raven.user_context(id: session[:current_user_id]) # or anything else in session
     Raven.extra_context(params: params.to_unsafe_h, url: request.url)
   end
+
+  def find_user
+   @user = User.find_by id: params[:id]
+    return if @user
+    flash[:danger] = t "flash.no_user"
+    redirect_to root_url
+  end
 end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,7 @@
+class UsersController < ApplicationController
+  before_action :find_user, only: [:show]
+
+  def show
+  end
+
+end
