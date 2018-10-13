5521983f3cbd4d64242e5126787a640b270fe88b
Nguyen Van Hanh <hanhnv.bkhn@gmail.com>
2018-10-13 16:28:07 +0000

    show game detail

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,4 +3,8 @@ class Game < ApplicationRecord
   has_many :genres
   has_many :screenshots
   mount_uploader :picture, PictureUploader
+  
+  def count_average_score
+    self.reviews.average(:score).round(2)
+  end
 end
