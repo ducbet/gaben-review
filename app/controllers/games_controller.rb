f5bc980fbf1a8d5b62a162db22baf8d921b34fc7
ducbet <ducbet1996@gmail.com>
2018-10-15 21:13:43 +0700

    add game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,7 +2,7 @@ class GamesController < ApplicationController
   def index
     @q = Game.ransack(params[:q])
     @games = @q.result.includes(:genres).page(params[:page]).per(3)
-    @genres = Genre.all.map(&:genre).uniq
+    @genres = Genre.genre_uniq
   end
 
   def show
@@ -10,5 +10,32 @@ class GamesController < ApplicationController
     @reviews = @game.reviews
     @comment = Review.new
     @new_reply = Reply.new
+  end
+
+  def new
+    @game = Game.new
+    @genres = Genre.genre_uniq
+  end
+
+  def create
+    @game = Game.new game_params
+    if @game.save
+      unless params[:screenshots].nil?
+        params[:screenshots]['picture'].each do |picture|
+          @screenshots = @game.screenshots.create!(:picture => picture)
+        end
+      end
+      flash[:success] = t "flash.add_game_success"
+      redirect_to @game
+    else
+      flash[:danger] = t "flash.add_game_failed"
+      render :new
+    end
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit :name, :details, :price, :picture, screenshots_attributes: [:id, :game_id, :picture]
   end
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -2,9 +2,13 @@ class Game < ApplicationRecord
   has_many :reviews
   has_many :genres
   has_many :screenshots
+  accepts_nested_attributes_for :screenshots, allow_destroy: true, reject_if: proc { |attributes| attributes['picture'].blank? }
   mount_uploader :picture, PictureUploader
 
+  validates :picture, presence: true
+
   def count_average_score
-    self.reviews.average(:score).round(2)
+    return 0 if reviews.empty?
+    reviews.average(:score).round(2)
   end
 end
